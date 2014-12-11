074078f196e85d9dcf7d7d570b45bcd43c53ea5e
Shathra <selcuk.gulcan@ug.bilkent.edu.tr>
2014-12-11 05:51:28 +0200

    CardPile changed

--- src/edu/ew/model/CardPile.java
+++ src/edu/ew/model/CardPile.java
@@ -1,5 +1,6 @@
 package edu.ew.model;
 
+import java.util.ArrayList;
 import java.util.Observable;
 
 /**
@@ -10,4 +11,23 @@ import java.util.Observable;
  */
 public class CardPile extends Observable{
 
+	private ArrayList<Card> cards;
+	
+	public CardPile() {
+		
+		cards = new ArrayList<Card>();
+	}
+	
+	public ArrayList<Card> getCards() {
+		return cards;
+	}
+
+	public void setCards(ArrayList<Card> cards) {
+		this.cards = cards;
+	}
+
+	public void addCard( Card card) {
+		
+		cards.add( card);
+	}
 }
