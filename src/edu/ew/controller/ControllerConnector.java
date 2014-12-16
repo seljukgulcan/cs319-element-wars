c14d3e60848d10912def358a112042c0b03aabd2
Shathra <selcuk.gulcan@ug.bilkent.edu.tr>
2014-12-17 00:50:11 +0200

    Connector classes

---
+++ src/edu/ew/controller/ControllerConnector.java
@@ -0,0 +1,12 @@
+package edu.ew.controller;
+
+/**
+ * 
+ * @author Selcuk Gulcan
+ *
+ * TODO: Add Description
+ */
+public class ControllerConnector {
+
+	//TODO: Complete the class.
+}
---
+++ src/edu/ew/model/ModelConnector.java
@@ -0,0 +1,12 @@
+package edu.ew.model;
+
+/**
+ * 
+ * @author Selcuk Gulcan
+ *
+ * TODO: Add Description
+ */
+public class ModelConnector {
+
+	//TODO: Complete the class.
+}
