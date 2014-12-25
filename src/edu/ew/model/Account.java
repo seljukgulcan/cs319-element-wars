8c6853b4e4dc906f84d28c6e7c7e07b5abe758cd
eforhus <sdemirkol@ug.bilkent.edu.tr>
2014-12-25 16:30:04 +0200

    Update Account.java

--- src/edu/ew/model/Account.java
+++ src/edu/ew/model/Account.java
@@ -5,10 +5,12 @@ import java.io.FileNotFoundException;
 import edu.ew.model.Player.Side;
 
 /**
+ * This class handles properties inside of the game like deck, charecter etc...
  * 
  * @author Selcuk Gulcan
+ * @author Serdar Demirkol
+ * @author Umut Hicyilmaz
  *
- *	TODO: Add Description
  */
 public class Account {
 
@@ -16,6 +18,12 @@ public class Account {
 	private Deck	deck;
 	private String  aiName;
 	
+	/**
+	 * Throws an exception if problem occurs with files
+	 * 
+	 * @exception java.io.FileNotFoundException if wrong file argument was passed
+	 * @exception java.io.CorruptedFileException if file is changed/corrupted unauthorized
+	 */
 	public Account() throws FileNotFoundException, CorruptedFileException {
 		
 		setName( Setting.getName());
@@ -23,6 +31,11 @@ public class Account {
 		setAIName( Setting.getAIName());
 	}
 	
+	/**
+	 * Creates account with given parameters
+	 * 
+	 * @param name
+	 */
 	public Account( String name) throws FileNotFoundException, CorruptedFileException {
 		
 		setDeck( DeckIO.importDeck( Setting.getDeckName()));
@@ -30,6 +43,11 @@ public class Account {
 		setAIName( Setting.getAIName());
 	}
 	
+	/**
+	 * Creates account with given parameters
+	 * 
+	 * @param deck
+	 */
 	public Account( Deck deck) throws FileNotFoundException {
 		
 		setDeck( deck);
@@ -37,6 +55,12 @@ public class Account {
 		setAIName( Setting.getAIName());
 	}
 	
+	/**
+	 * Creates account with given parameters
+	 * 
+	 * @param name
+	 * @param deck
+	 */
 	public Account( String name, Deck deck) throws FileNotFoundException {
 		
 		setDeck( deck);
@@ -44,33 +68,68 @@ public class Account {
 		setAIName( Setting.getAIName());
 	}
 	
+	/**
+	 * Creates player with given parameters
+	 * 
+	 * @param side
+	 */
 	public Player createPlayer( Side side) {
 		
 		return new Player( side, deck);
 	}
 
 	//TRIVIAL METHODS
+	/**
+	 * Gets name
+	 * 
+	 * @return name
+	 */
 	public String getName() {
 		return name;
 	}
-
+	
+	/**
+	 * Gets deck
+	 * 
+	 * @return deck
+	 */
 	public Deck getDeck() {
 		return deck;
 	}
 	
+	/**
+	 * Gets aiName
+	 * 
+	 * @return aiName
+	 */
 	public String getAIName() {
 		
 		return aiName;
 	}
-
+	
+	/**
+	 * Sets name
+	 * 
+	 * @param name
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
-
+	
+	/**
+	 * Sets deck
+	 * 
+	 * @param deck
+	 */
 	public void setDeck(Deck deck) {
 		this.deck = deck;
 	}
 	
+	/**
+	 * Sets aiName
+	 * 
+	 * @param aiName
+	 */
 	public void setAIName( String aiName) {
 		
 		this.aiName = aiName;
